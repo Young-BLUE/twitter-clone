f4149005ab8043b942fea70b9416628153095917
Young-BLUE <youngblue.coding@gmail.com>
2023-01-28 17:27:12 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwQLocq9uAMJz74S7L6eNC0s-HaEton9E",
+    authDomain: "twitter-clone-7c238.firebaseapp.com",
+    projectId: "twitter-clone-7c238",
+    storageBucket: "twitter-clone-7c238.appspot.com",
+    messagingSenderId: "990908488330",
+    appId: "1:990908488330:web:5d4fc311040681afc0ab73"
+};
+
+export default initializeApp(firebaseConfig);
