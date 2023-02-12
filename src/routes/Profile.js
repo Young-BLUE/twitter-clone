02d41a06568853a9674a2e1e5215946c8de4878f
young-blue <youngblue.coding@gmail.com>
2023-02-12 19:12:25 +0900

    profile 페이지에서 내가 등록한 트윗의 정보 가져오기 쿼리 추가 where 절 사용하여 필터링

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { authService } from "../firebaseInstance";
+import React, { useEffect } from "react";
+import { authService, dbCollection, dbService } from "../firebaseInstance";
 import { useNavigate } from "react-router-dom";
+import { orderBy, query, where, getDocs } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+  const getMyDtweets = async () => {
+    const q = query(
+      dbCollection(dbService, "dtweets"),
+      where("creatorId", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyDtweets();
+  }, []);
   return <button onClick={onLogOutClick}>Log Out</button>;
 };
 
